6fb73ba49c72456efd848d10d6b51e5b220692a8
olafaloofian <bulluffalo@gmail.com>
2019-06-05 02:39:52 -0700

    Fix portfolio item page

--- src/Components/Portfolio/PortfolioItem.js
+++ src/Components/Portfolio/PortfolioItem.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { withContext } from '../../ContextAPI/ContextHOC'
+import LoadingDots from '../../Assets/LoadingDots/LoadingDots'
 import './Portfolio.scss'
 
-export default class PortfolioItem extends Component {
+class PortfolioItem extends Component {
     state = {
         allItems: null,
         selectedItem: null
     }
 
-    componentDidMount() {
-        this.getDataFromSessionStorage()
-    }
+    // componentDidMount() {
+    //     this.getDataFromSessionStorage()
+    // }
 
     // Get data from session storage and select one based off of route id parameter
-    getDataFromSessionStorage = () => {
-        const portfolioItems = JSON.parse(sessionStorage.getItem('portfolioItems'))
-        const filteredItem = portfolioItems.find(item => item.id === +this.props.match.params.id)
+    // getDataFromSessionStorage = () => {
+    //     const portfolioItems = this.props.context.userImages.portfolio
+    //     const filteredItem = portfolioItems.find(item => item.project_id === +this.props.match.params.id)
 
-        this.setState({
-            allItems: portfolioItems,
-            selectedItem: filteredItem
-        })
-    }
+    //     this.setState({
+    //         allItems: portfolioItems,
+    //         selectedItem: filteredItem
+    //     })
+    // }
 
     // Handles same-component linking to render new data
     componentDidUpdate(prevProps) {
-        if(prevProps.match.params.id !== this.props.match.params.id) {
-            this.getDataFromSessionStorage()
+        const { userImages } = this.props.context
+        if(prevProps.match.params.id !== this.props.match.params.id || prevProps.context.userImages !== userImages) {
+            this.setState({
+                selectedItem: userImages.portfolio.find(project => project.id === this.props.match.params.id),
+                allItems: userImages.portfolio
+            })
             window.scrollTo(0, 0)
+        }
+    }
+
+    nextProject = (selectedItem, isForward) => {
+        const { allItems } = this.state
+        const index = allItems.findIndex(e => e === selectedItem)
+        if (isForward) {
+            if (index === allItems.length - 1) {
+                return allItems[0].id
+            } else {
+                return allItems[index + 1].id
+            }
+        } else {
+            if (index === 0) {
+                return allItems[allItems.length - 1].id
+            } else {
+                return allItems[index - 1].id
+            }
         }
     }
 
@@ -37,25 +61,23 @@ export default class PortfolioItem extends Component {
         return (
             <div className='portfolio-item-container'>
                 {allItems ?
-                // Render main if the state has been set successfully
                     <section>
-                        {this.state.selectedItem.pictureUrls.map((picture, index) => {
+                        {selectedItem.images.map((image, index) => {
                             return (
-                                <img src={picture} alt="" key={index}/>
+                                <img src={image.url} alt="" key={index}/>
                             )
                         })}
                         <div className="portfolio-item-description">
-                            {this.state.selectedItem.description}
                         </div>
                         <div className='portfolio-item-footer'>
                             {/* The <a> tag reloads the page, and pictures show up one by one */}
-                            <Link to={`/portfolio/${selectedItem === allItems[0] ? allItems.slice(-1)[0].id : selectedItem.id - 1}`}>
+                            <Link to={`/portfolio/${this.nextProject(selectedItem, false)}`}>
                                 <nav>
                                     ◄ PREVIOUS PROJECT
                                 </nav>
                             </Link>
                             {/* The <Link> keeps the user at the bottom and loads new images. Ask Sydney which is better. */}
-                            <Link to={`/portfolio/${selectedItem === allItems.slice(-1)[0] ? allItems[0].id : selectedItem.id + 1}`}>
+                            <Link to={`/portfolio/${this.nextProject(selectedItem, true)}`}>
                                 <nav>
                                     NEXT PROJECT ►
                                 </nav>
@@ -64,10 +86,12 @@ export default class PortfolioItem extends Component {
                     </section>
                     :
                     // Loading indicator
-                    <img src="https://media2.giphy.com/media/9D2iQLabobsME/giphy.gif?cid=3640f6095c0ffa59457759714d022797" alt=""/>
+                    <div className='loader'><LoadingDots /></div>
                 }
                 
             </div>
         );
     }
 }
+
+export default withContext(PortfolioItem)
